4eb6b79bf6c9335d939feaf04bba47439f141b72
Daniel Earl <danielearl392@gmail.com>
2025-06-03 21:49:26 +0100

    Update promotion and relegation Update new Everton ground Add unit tests

---
+++ The 92Tests/The_92Tests.swift
@@ -0,0 +1,17 @@
+//
+//  The_92Tests.swift
+//  The 92Tests
+//
+//  Created by Daniel Earl on 29/05/2025.
+//  Copyright © 2025 Daniel Earl. All rights reserved.
+//
+
+import Testing
+
+struct The_92Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
